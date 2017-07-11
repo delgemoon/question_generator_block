e4c2db0e6c8cdee356c2890fb1936bd64e3e4140
dqcanh <dqcanh@gmail.com>
2017-07-11 18:52:37 +0700

    add method to check answer from 2 files (Student Answer ans Teacher Answer spreadsheets)

--- question_generator_block/google_service.py
+++ question_generator_block/google_service.py
@@ -10,6 +10,8 @@ from oauth2client.file import Storage
 from oauth2client.service_account import ServiceAccountCredentials
 
 from pprint import pprint
+import re
+
 # from googleapiclient import discovery
 
 # try:
@@ -190,6 +192,106 @@ class gsheets():
         return False
 
 
+    def check_answer_sheet(self, student_answer_spreadsheetId, teacher_answer_spreadsheetId, data_range):
+        '''
+        Validate answer base on answer spreadsheets of student and teacher from google spreadsheets (URL)
+
+        :param student_answer_spreadsheetId: https://docs.google.com/spreadsheets/d/15Hlu3jCJjEzKgvMdMle5c2KwEri2HGDSQKiLbh_V8JU/edit
+        :param teacher_answer_spreadsheetId: https://docs.google.com/spreadsheets/d/1uQfNkOefkDBHFIPsa_I-Bo8VgLzUPOu4faaEhLk0hT8/edit
+        :return: boolean True/False
+
+        :author: canhdq
+        '''
+
+        result = False
+
+        print('Teacher answer spreadsheetId:' + teacher_answer_spreadsheetId)
+        print('Student answer spreadsheetId:' + student_answer_spreadsheetId)
+
+        # # format answer input strings:
+        # formatted_url_teacher_answer = url_teacher_answer.replace(' ', '')  # get rig of space if any
+        #
+        # spreadsheetId_teacher_answer = formatted_url_teacher_answer.split('/d/') #
+        # spreadsheetId_teacher_answer = spreadsheetId_teacher_answer[-1]
+        # spreadsheetId_teacher_answer = re.sub()
+        #
+        # student_answer = student_answer.replace(' ', '')  # get rig of space character
+        # student_answer_list = student_answer.split('=')
+        # student_answer = '=' + student_answer_list[-1]
+        #
+        # print('Expected teacher answer:' + teacher_answer)
+        # print('Expected student answer:' + student_answer)
+
+
+        # credentials = get_credentials()   # use client ID
+        credentials = get_credentials_by_service_account()  # use service account
+
+        http = credentials.authorize(httplib2.Http())
+        discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                        'version=v4')
+        service = discovery.build('sheets', 'v4', http=http,
+                                  discoveryServiceUrl=discoveryUrl)
+
+        # Read answer data from spreadsheet
+        #
+
+        # The A1 notation of answer data
+        range_student_answer = data_range
+        range_teacher_answer = data_range
+
+        # How values should be represented in the output.
+        # The default render option is ValueRenderOption.FORMATTED_VALUE.
+        value_render_option = 'UNFORMATTED_VALUE'  # TODO: Update placeholder value.
+
+        # How dates, times, and durations should be represented in the output.
+        # This is ignored if value_render_option is
+        # FORMATTED_VALUE.
+        # The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+        date_time_render_option = 'FORMATTED_STRING'  # TODO: Update placeholder value.
+
+        # get student answer file
+        #
+        spreadsheetId = student_answer_spreadsheetId
+        range_ = range_student_answer  # TODO: Update placeholder value.
+
+        request = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_,
+                                                      valueRenderOption=value_render_option,
+                                                      dateTimeRenderOption=date_time_render_option)
+        response = request.execute()
+        # extract value
+        student_answer_value = response['values'][0][0]
+        print('Student answer data:')
+        pprint(response)
+
+        # get teacher answer file
+        #
+        spreadsheetId = teacher_answer_spreadsheetId
+        range_ = range_teacher_answer  # TODO: Update placeholder value.
+
+        request = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_,
+                                                      valueRenderOption=value_render_option,
+                                                      dateTimeRenderOption=date_time_render_option)
+        response = request.execute()
+        # extract value
+        teacher_answer_value = response['values'][0][0]
+        print('Teacher answer data:')
+        pprint(response)
+
+        print('Teacher answer value:')
+        pprint(teacher_answer_value)
+        print('Student answer value:')
+        pprint(student_answer_value)
+
+        if student_answer_value == teacher_answer_value:
+            print('Correct')
+            result = True
+        else:
+            print('Inccorrect')
+            result = False
+
+        return result
+
+
 
 def main():
 
@@ -236,11 +338,22 @@ def main():
     # # TODO: Change code below to process the `response` dict:
     # pprint(response)
 
-    print('Calling Google Spreadsheets to evaluate student answer...')
+    print('Calling Google Spreadsheets to check answer...')
     sheets = gsheets()
-    teacher_answer = 'x=SUM(5,9)'
-    student_answer = 'x = 5 -9'
-    sheets.check_answer(teacher_answer,student_answer)
+
+    # teacher_answer = 'x=SUM(5,9)'
+    # student_answer = 'x = 5 -9'
+    # sheets.check_answer(teacher_answer,student_answer)
+
+    # student_spreadsheetId = '1sxt5Eo9NqOlHa3lbK7-hAgVm3To5D5W_s4X6WNFChBE'
+    # teacher_spreadsheetId = '1sxt5Eo9NqOlHa3lbK7-hAgVm3To5D5W_s4X6WNFChBE'
+
+    student_spreadsheetId = '1uQfNkOefkDBHFIPsa_I-Bo8VgLzUPOu4faaEhLk0hT8'
+    teacher_spreadsheetId = '1uQfNkOefkDBHFIPsa_I-Bo8VgLzUPOu4faaEhLk0hT8'
+
+    data_range = "A2"
+    sheets.check_answer_sheet(teacher_spreadsheetId, student_spreadsheetId, data_range)
+
 
 
 
